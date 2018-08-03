0f8f0970e9b57a9fa9a35b1702c993344b74b7e7
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-08-02 23:12:40 -0400

    Add task to relocate all contents in icons folder to dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,12 @@
-//PROJECT OVERVIEW: 
-    // The build process must fulfill the following criteria:
+//7) gulp build command at the command line to run the clean, scripts, 
+//styles, and images tasks with confidence that the clean task completes before the other commands.
 
-    // Concatenate and minify the JavaScript files
-    // Compile SCSS into CSS in a concatenated and minified file
-    // Generate JavaScript and CSS source maps
-    // Compress any JPEG or PNG files
-    // All output for the build process should be in a dist folder for distribution or deployment.
-
-// Steps to Take: 
-    //1) npm install should install all of the dependencies for the build process.
-
-    //2)run the gulp scripts command at the command line to 
-        //concatenate, minify, and copy all of the project’s JavaScript files into an all.min.js file that is then copied to the dist/scripts folder.
-
-    //3)gulp styles command at the command line to compile the project’s 
-        //SCSS files into CSS, then concatenate and minify into an all.min.css file that is then copied to the dist/styles folder.
-
-    //4) gulp scripts or gulp styles commands at the command line, source maps are generated for the JavaScript and CSS files respectively.
-
-    //5) gulp images command at the command line to optimize the size of the project’s
-        // JPEG and PNG files, and then copy those optimized images to the dist/content folder.
-
-    //6) I should be able to run the gulp clean command at the command line 
-        //to delete all of the files and folders in the dist folder.
-
-    //7) gulp build command at the command line to run the clean, scripts, 
-        //styles, and images tasks with confidence that the clean task completes before the other commands.
-
-    //8)should be able to run the gulp command at the command line to run the build task and serve my project using a local web server.
+//8)should be able to run the gulp command at the command line to run the build task and serve my project using a local web server.
 
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify'); //for minifying JS files
-const concat = require('gulp-concat'); //combine files
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const maps = require('gulp-sourcemaps');
 const del = require('del');
@@ -47,7 +21,7 @@ gulp.task('concatScripts', () => {
         'js/circle/circle.js'])
             .pipe(maps.init())
             .pipe(concat('global.js'))
-            .pipe(maps.write('./')) //same dir as global.js
+            .pipe(maps.write('./'))
             .pipe(gulp.dest('js'));
 });
 
@@ -85,18 +59,18 @@ gulp.task('images', () => {
         .pipe(gulp.dest('dist/images'));
 });
 
-// ICONS compy the icons folder and the subfolder, must contain all files
 gulp.task('icons', () => {
-    gulp.src(['icons/', 'icons/svg/'])
+    gulp.src(['icons/**'])
+        .pipe(gulp.dest('dist/icons'));
 })
+
 
 gulp.task("clean", () => {
     del(['dist', 'js/global*.js*', 'styles']);
 });
 
-//runs the tasks to concat/minify assets and moves html and icons to the dist directory
-gulp.task('build', ['scripts', 'styles', 'images'], () => {
-    gulp.src(['index.html', 'icons'])
+gulp.task('build', ['scripts', 'styles', 'images', 'icons'], () => {
+    gulp.src(['index.html'])
     .pipe(gulp.dest('dist'));
 
     //start the server
